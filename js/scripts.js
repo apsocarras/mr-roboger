b3476708ec2c27dd764d1476e39eab89a5fb6e2b
Alejandro Socarras <apsocarras@gmail.com>
2022-10-28 12:20:00 -0600

    Complete third test for robogerify()

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 // Business Logic
 
 function robogerify(number) {
-
   let numArray = [];
   for (let i = 0; i <= number; i++) {
     numArray.push(i);
   }
-
   let roboArray = [];
   numArray.forEach(function(num) {
     if (num.toString().includes("3")) {
       roboArray.push("Won't you be my neighbor?");
+    } else if (num.toString().includes("2")) { 
+      roboArray.push("Boop!");
     } else {
       roboArray.push(num);
     }
